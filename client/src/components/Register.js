744051826fb8a5f78db9c4f5c8e58629f71b0ce8
jakeaston <jacobaston92@gmail.com>
2021-02-22 11:05:25 +0000

    Login and register pages complete

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
-const Register = () => {
-  return <h1>Register</h1>
+import LooseForm from '././LooseForm.js'
+
+function Register({ history }) {
+
+  const [formData, updateFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    image: '',
+    age: '',
+    homeTown: '',
+    interests: []
+  })
+
+  function handleChange(event) {
+    updateFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const newFormData = {
+      ...formData,
+      types: formData.interests.map(type => type.value)
+    }
+
+    try {
+      const { data } = await axios.post('/api/register', newFormData, )
+      console.log(data._id)
+      history.push('/login')
+    } catch (err) {
+      console.log(err.response.data)
+    }
+  }
+
+  return <LooseForm
+    handleChange={handleChange}
+    handleTypeChange={(interests) => updateFormData({ ...formData, interests })}
+    handleSubmit={handleSubmit}
+    formData={formData}
+  />
 }
 
 export default Register
