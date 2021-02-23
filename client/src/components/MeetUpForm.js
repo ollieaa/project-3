19b69772a06c1d8a98b56b0434aafa4be5887af0
Ollie Anderson <ollieaa17@gmail.com>
2021-02-23 12:20:02 +0000

    navbar profile dropdown

---
+++ client/src/components/MeetUpForm.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const inputFields = ['name', 'location', 'image', 'description']
+
+export default function GroupForm({ formData, handleSubmit, handleChange }) {
+  return <div className="section">
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+
+        {inputFields.map(field => {
+          return <div key={field} className="field">
+            <label className="label">
+              {field[0].toUpperCase() + field.slice(1)}
+            </label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                value={formData[field]}
+                onChange={handleChange}
+                name={field}
+              />
+            </div>
+          </div>
+        })}
+
+        <button className="button mt-5 is-success">Submit</button>
+      </form>
+    </div>
+  </div>
+}
