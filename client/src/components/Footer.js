8f9dd14b15ec950a43f2730bd9f0bc0cdaf73598
Katherine Herbert <katherinekherbert@gmail.com>
2021-02-25 15:15:08 +0100

    kh thurs afternoon

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -22,6 +22,9 @@ const Footer = () => {
       <Link to={'/activities'}><h4 className="subtitle is-size-6 mb-1">Restaurants</h4></Link>
       <Link to={'/groups'}><h4 className="subtitle is-size-6 mb-1">Groups</h4></Link>
       <Link to={'/poi'}><h4 className="subtitle is-size-6 mb-1">Points of Interest</h4></Link>
+      <Link to ={'/map'}><h4 className="subtitle is-size-6 mb-1">Map</h4></Link>
+
+
     </div>
     <div className="column">
       <h2 className="title mt-5 mb-5">Visit</h2>
--- client/src/data/poiTypes.js
+++ client/src/data/poiTypes.js
@@ -2,16 +2,16 @@ export default [
   { value: 'gallery', label: 'Galleries' },
   { value: 'gardens', label: 'Gardens' },
   { value: 'landmark', label: 'Landmarks' },
-  { value: 'market', label: 'Market' },
+  { value: 'market', label: 'Markets' },
   { value: 'monument', label: 'Monuments' },
   { value: 'museum', label: 'Museums' },
   { value: 'other', label: 'Other' },
   { value: 'palace', label: 'Palaces' },
-  { value: 'park', label: 'Park' },
+  { value: 'park', label: 'Parks' },
   // { value: 'public', label: 'Public Art' },
-  { value: 'religious', label: 'Religious Building' },
+  { value: 'religious', label: 'Religious Buildings' },
   { value: 'sports', label: 'Sports' },
-  { value: 'square', label: 'Square' },
-  { value: 'statue', label: 'Statue' },
-  { value: 'theatre', label: 'Theatre' }
+  { value: 'square', label: 'Squares' },
+  { value: 'statue', label: 'Statues' },
+  { value: 'theatre', label: 'Theatres' }
 ]
