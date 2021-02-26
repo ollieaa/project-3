c7c8b4ec11d0efff2c12b56f209e35340c53cab0
Ollie Anderson <ollieaa17@gmail.com>
2021-02-26 14:34:46 +0000

    deployment

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar({ history }) {
 
   return <nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
-      <Link className="navbar-item" to={'/home'}>
+      <Link className="navbar-item" to={'/'}>
         <img src={logo} />
       </Link>
       <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -42,7 +42,7 @@ function Navbar({ history }) {
     </div>
     <div id="navbarBasicExample" className="navbar-menu">
       <div className="navbar-start">
-        <Link className="navbar-item" to={'/home'}>
+        <Link className="navbar-item" to={'/'}>
           Home
         </Link>
         {!loggedInUser._id &&
